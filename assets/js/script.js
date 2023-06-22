1fb75fe0189d94e0098b6cff9ce60f0ac1ec9c93
Simon Darling <133539244+DarlingDC2@users.noreply.github.com>
2023-06-22 16:45:09 -0400

    base code

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// start screen with a button to begin and button to view hs
+// when start button is pressed question 1 appears with four buttons 
+// 3 out of 4 buttons will return false
+// 1 will return true
+// timer begins when start button is pressed with 90 seconds countdown
+// if button pressed is false remove 15 seconds
+// when answer pressed bring next question with same rules
+// repeat with 5 questions  
+// end of 5 questions stop timer/ remove timer element
+// record time as score
+// show score and include prompt to enter initials
+// when initials entered move to highscore screen
+// remove view highscores button and timer on hs screen
+// save initials and score(time) to local 
+// order initials by highest score to lowest
